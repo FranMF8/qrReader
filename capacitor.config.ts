95d4e8c6637740395b4617cca4f5fb4df15eee18
Francisco Fernandez <cejita678@hotmail.com>
2024-12-30 08:10:44 -0300

    rawr

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'qrReader',
+  appId: 'com.QRGEN.app',
+  appName: 'Qr Generator',
   webDir: 'www'
 };
 
---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  // Carga la aplicación Ionic
+  mainWindow.loadFile(path.join(__dirname, 'www/index.html'));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
